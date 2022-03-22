c71a44622ccbd6f1dfc75930047719f86609a0fc
israa <israa-osta@hotmail.com>
2022-03-22 19:03:07 +0200

    after edit assig internal styling to one templet

--- _python/flask/flask_fundimantal/Internal Styling/server.py
+++ _python/flask/flask_fundimantal/Internal Styling/server.py
@@ -5,15 +5,15 @@ def index0():
     return "hello"
 @app.route('/play')
 def index1():
-    return render_template('index.html')
+    return render_template('index.html',countdiv=3,countt=1,page=1)
 @app.route('/play/<count>')
 def index2(count):
     count=int(count)
-    return render_template('index2.html',countdiv=count,countt=int(count/4))
+    return render_template('index.html',countdiv=count,countt=int(count/4),page=2)
 @app.route('/play/<x>/<color>')
 def index3(x,color):
     x=int(x)
-    return render_template('index3.html',countdiv=x,countt=int(x/4),Bcolor=color)
+    return render_template('index.html',countdiv=x,countt=int(x/4),Bcolor=color,page=3)
 
 if __name__=="__main__":
     app.run(debug=True)
